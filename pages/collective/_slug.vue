b50f9901dcd79083c4b5e3a811135afdcb960324
madebyporter <general@madebyporter.com>
2022-09-10 03:45:36 -0700

    added member if statements

--- pages/collective/_slug.vue
+++ pages/collective/_slug.vue
@@ -1,30 +1,38 @@
 <template>
   <section>
     <h1>{{ post.fields.name }}</h1>
-    <p>{{ post.fields.title }}</p>
-    <p>{{ post.fields.bio }}</p>
-    <p><img :src="`${post.fields.avatar.fields.file.url}`" /></p>
+    <p v-if="post.fields.title">{{ post.fields.title }}</p>
+    <p v-if="post.fields.bio">{{ post.fields.bio }}</p>
+    <p v-if="post.fields.avatar"><img :src="`${post.fields.avatar.fields.file.url}`" /></p>
 
-    <h2>Links:</h2>
-    <ul>
-      <li v-for="link in post.fields.links">
-        <a :href="`${link}`">{{ link }}</a>
-      </li>
-    </ul>
+    <div v-if="post.fields.links">
+      <h2>Links:</h2>
+      <ul>
+        <li v-for="link in post.fields.links">
+          <a :href="`${link}`">{{ link }}</a>
+        </li>
+      </ul>
+    </div>
+    
+    <div v-if="post.fields.disciplines">
+      <h2>Disciplines:</h2>
+      <ul>
+        <li v-for="discipline in post.fields.disciplines">
+          {{ discipline }}
+        </li>
+      </ul>
+    </div>
 
-    <h2>Disciplines:</h2>
-    <ul>
-      <li v-for="discipline in post.fields.disciplines">
-        {{ discipline }}
-      </li>
-    </ul>
+    <div v-if="post.fields.verticals">
+      <h2>Verticals:</h2>
+      <ul>
+        <li v-for="vertical in post.fields.verticals">
+          {{ vertical }}
+        </li>
+      </ul>
+    </div>
 
-    <h2>Verticals:</h2>
-    <ul>
-      <li v-for="vertical in post.fields.verticals">
-        {{ vertical }}
-      </li>
-    </ul>
+    
   </section>
 </template>
 
