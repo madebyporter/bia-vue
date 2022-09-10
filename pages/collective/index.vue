663a05523bb7a02f74e2fa5796ddb7cd0c96e8a9
madebyporter <general@madebyporter.com>
2022-09-10 03:58:38 -0700

    fixed nuxtlink members slug

--- pages/collective/index.vue
+++ pages/collective/index.vue
@@ -5,8 +5,8 @@
     </section>
     <section class="content" >
       <ul>
-        <li v-for="post in posts" :key="post.fields.slug" @click="$router.push(`/collective/${post.fields.slug}`)">
-          <h2>{{ post.fields.name }}</h2>
+        <li v-for="post in posts" :key="post.fields.slug">
+          <h2><NuxtLink :to="/collective/+`${post.fields.slug}`">{{ post.fields.name }}</NuxtLink></h2>
           <p>{{ post.fields.title }}</p>
         </li>
       </ul>
