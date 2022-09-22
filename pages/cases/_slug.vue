376be8739189568da5d3b3a3dc3b324f16e9d99a
madebyporter <general@madebyporter.com>
2022-09-22 16:59:49 -0700

    added new case content

--- pages/cases/_slug.vue
+++ pages/cases/_slug.vue
@@ -1,34 +1,50 @@
 <template>
   <section>
     <h1>{{ cases.fields.title }}</h1>
+    <div v-if="cases.fields.featuredImage"><img :src="`${cases.fields.featuredImage.fields.file.url}`" /></div>
     <p v-if="cases.fields.type">{{ cases.fields.type }}</p>
-    <p v-if="cases.fields.summary">{{ cases.fields.summary }}</p>
-    <div v-if="cases.fields.delivarables">
+    <aside v-if="cases.fields.delivarables">
       <h2>Deliverables:</h2>
       <ul>
         <li v-for="delivarables in cases.fields.delivarables">
           {{ delivarables }}
         </li>
       </ul>
-    </div>
-    <div v-if="cases.fields.team">
+    </aside>
+    <aside v-if="cases.fields.team">
       <h2>Team:</h2>
       <ul>
         <li v-for="team in cases.fields.team">
           {{ team.fields.name }}
         </li>
       </ul>
-    </div>
-    <div v-if="cases.fields.disciplines">
+    </aside>
+    <aside v-if="cases.fields.disciplines">
       <h2>Disciplines:</h2>
       <ul>
         <li v-for="disciplines in cases.fields.disciplines">
           {{ disciplines.fields.title }}
         </li>
       </ul>
+    </aside>
+
+    <div v-if="cases.fields.challenge">
+      <h2>Challenge:</h2>
+      <ul>
+        <p v-if="cases.fields.challenge">{{ cases.fields.challenge }}</p>
+      </ul>
     </div>
-    
-    <p v-if="cases.fields.featuredImage"><img :src="`${cases.fields.featuredImage.fields.file.url}`" /></p>
+
+    <div v-if="cases.fields.solution">
+      <h2>Solution:</h2>
+      <ul>
+        <p v-if="cases.fields.solution">{{ cases.fields.solution }}</p>
+      </ul>
+    </div>
+
+    <div v-if="cases.fields.bodyImage"><img :src="`${cases.fields.bodyImage.fields.file.url}`" /></div>
+
+    <div v-if="cases.fields.bodyContent" v-html="$md.render(cases.fields.bodyContent)"></div>
 
     <div v-if="cases.fields.postImages">
       <h2>postImages:</h2>
