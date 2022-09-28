9cc90b7ebbec6a282d428d2521eaf38e4f3e7cad
madebyporter <general@madebyporter.com>
2022-09-28 15:52:29 -0700

    added case styles desktop

--- pages/cases/_slug.vue
+++ pages/cases/_slug.vue
@@ -1,81 +1,122 @@
 <template>
-  <section class="section">
-    <div class="content-full">
-      <h1 class="h1">{{ cases.fields.title }}</h1>
-      <div v-if="cases.fields.featuredImage"><img :src="`${cases.fields.featuredImage.fields.file.url}`" /></div>
-      <p v-if="cases.fields.type">{{ cases.fields.type }}</p>
-      <aside v-if="cases.fields.delivarables">
-        <h2>Deliverables:</h2>
-        <ul>
-          <li v-for="delivarables in cases.fields.delivarables">
-            {{ delivarables }}
-          </li>
-        </ul>
-      </aside>
-      <aside>
-        <h2>Team:</h2>
-        <ul>
-          <li v-if="cases.fields.teamMemberA">
-            <p v-if="cases.fields.teamMemberA.fields.avatar"><img :src="`${cases.fields.teamMemberA.fields.avatar.fields.file.url}`" /></p>
-            <h3>{{ cases.fields.teamMemberA.fields.name }}</h3>
-            <p>{{ cases.fields.teamRoleA.fields.title }}</p>
-          </li>
-          <li v-if="cases.fields.teamMemberB">
-            <p v-if="cases.fields.teamMemberB.fields.avatar"><img :src="`${cases.fields.teamMemberB.fields.avatar.fields.file.url}`" /></p>
-            <h3>{{ cases.fields.teamMemberB.fields.name }}</h3>
-            <p>{{ cases.fields.teamRoleB.fields.title }}</p>
-          </li>
-          <li v-if="cases.fields.teamMemberC">
-            <p v-if="cases.fields.teamMemberC.fields.avatar"><img :src="`${cases.fields.teamMemberC.fields.avatar.fields.file.url}`" /></p>
-            <h3>{{ cases.fields.teamMemberC.fields.name }}</h3>
-            <p>{{ cases.fields.teamRoleC.fields.title }}</p>
-          </li>
-          <li v-if="cases.fields.teamMemberD">
-            <p v-if="cases.fields.teamMemberD.fields.avatar"><img :src="`${cases.fields.teamMemberD.fields.avatar.fields.file.url}`" /></p>
-            <h3>{{ cases.fields.teamMemberD.fields.name }}</h3>
-            <p>{{ cases.fields.teamRoleD.fields.title }}</p>
-          </li>
-        </ul>
-      </aside>
-
-      <div v-if="cases.fields.challenge">
-        <h2>Challenge:</h2>
-        <ul>
-          <p v-if="cases.fields.challenge">{{ cases.fields.challenge }}</p>
-        </ul>
+  <div>
+    <section class="section-large">
+      <div class="content-full">
+        <h1 class="h1 h-mb-0">{{ cases.fields.title }}</h1>
       </div>
+    </section>
 
-      <div v-if="cases.fields.solution">
-        <h2>Solution:</h2>
-        <ul>
-          <p v-if="cases.fields.solution">{{ cases.fields.solution }}</p>
+    <section class="section-xy0">
+      <div class="content-wide">
+        <div class="hero-image" v-if="cases.fields.featuredImage"><img :src="`${cases.fields.featuredImage.fields.file.url}`" /></div>
+      </div>
+    </section>
+
+    <section class="section">
+      <div class="content-sidebar">
+        <ul class="list-default">
+          <li><h3 v-if="cases.fields.type">{{ cases.fields.type }}</h3></li>
         </ul>
+
+        <aside v-if="cases.fields.delivarables">
+          <h2 class="list-header h6">Deliverables</h2>
+          <ul class="list-default">
+            <li v-for="delivarables in cases.fields.delivarables">
+              {{ delivarables }}
+            </li>
+          </ul>
+        </aside>
+
+        <aside>
+          <h2 class="list-header h6">Team</h2>
+          <ul class="list-default">
+            <li v-if="cases.fields.teamMemberA">
+              <div class="list-avatar">
+                <p class="avatar-small" v-if="cases.fields.teamMemberA.fields.avatar"><img :src="`${cases.fields.teamMemberA.fields.avatar.fields.file.url}`" /></p>
+              </div>
+              <div class="list-content person">
+                <h3 class="h-mb-0 person-name">{{ cases.fields.teamMemberA.fields.name }}</h3>
+                <p class="person-role">{{ cases.fields.teamRoleA.fields.title }}</p>
+              </div>
+            </li>
+            <li v-if="cases.fields.teamMemberB">
+              <div class="list-avatar">
+                <p class="avatar-small" v-if="cases.fields.teamMemberB.fields.avatar"><img :src="`${cases.fields.teamMemberB.fields.avatar.fields.file.url}`" /></p>
+              </div>
+              <div class="list-content person">
+                <h3 class="h-mb-0 person-name">{{ cases.fields.teamMemberB.fields.name }}</h3>
+                <p class="person-role">{{ cases.fields.teamRoleB.fields.title }}</p>
+              </div>
+            </li>
+            <li v-if="cases.fields.teamMemberC">
+              <div class="list-avatar">
+                <p class="avatar-small" v-if="cases.fields.teamMemberC.fields.avatar"><img :src="`${cases.fields.teamMemberC.fields.avatar.fields.file.url}`" /></p>
+              </div>
+              <div class="list-content person">
+                <h3 class="h-mb-0 person-name">{{ cases.fields.teamMemberC.fields.name }}</h3>
+                <p class="person-role">{{ cases.fields.teamRoleC.fields.title }}</p>
+              </div>
+            </li>
+            <li v-if="cases.fields.teamMemberD">
+              <div class="list-avatar">
+                <p class="avatar-small" v-if="cases.fields.teamMemberD.fields.avatar"><img :src="`${cases.fields.teamMemberD.fields.avatar.fields.file.url}`" /></p>
+              </div>
+              <div class="list-content person">
+                <h3 class="h-mb-0 person-name">{{ cases.fields.teamMemberD.fields.name }}</h3>
+                <p class="person-role">{{ cases.fields.teamRoleD.fields.title }}</p>
+              </div>
+            </li>
+          </ul>
+        </aside>
       </div>
+      
+      <div class="content-post-sidebar">
+        <div class="content-inner" v-if="cases.fields.challenge">
+          <h2 class="content-header h6">Challenge:</h2>
+          <div class="p-large" v-if="cases.fields.challenge">{{ cases.fields.challenge }}</div>
+        </div>
 
-      <div v-if="cases.fields.bodyImage"><img :src="`${cases.fields.bodyImage.fields.file.url}`" /></div>
-
-      <div v-if="cases.fields.bodyContent" v-html="$md.render(cases.fields.bodyContent)"></div>
-
-      <div v-if="cases.fields.postImages">
-        <h2>Carousel Images:</h2>
-        <ul>
-          <li v-for="postImage in cases.fields.postImages">
-            <img :src="`${postImage.fields.file.url}`" />
-          </li>
-        </ul>
+        <div class="content-inner" v-if="cases.fields.solution">
+          <h2 class="content-header h6">Solution:</h2>
+          <div class="p-large" v-if="cases.fields.solution">{{ cases.fields.solution }}</div>
+        </div>
       </div>
+    </section>
 
-      <div v-if="cases.fields.finalMockupImage">
-        <h2>Final Mockup Image:</h2>
-        <img :src="`${cases.fields.finalMockupImage.fields.file.url}`" />
+    <section class="section">
+      <div class="content-wide">
+        <div v-if="cases.fields.bodyImage"><img class="image-rounded" :src="`${cases.fields.bodyImage.fields.file.url}`" /></div>
       </div>
-    </div>
-  </section>
+    </section>
+
+    <section class="section-large">
+      <div class="content-full">
+        <div class="p-large" v-if="cases.fields.bodyContent" v-html="$md.render(cases.fields.bodyContent)"></div>
+      </div>
+    </section>
+
+    <section class="section-xy0">
+      <div class="content-wide">
+        <div class="carousel">
+          <ul class="carousel-track">
+            <li class="carousel-element" v-for="postImage in cases.fields.postImages">
+              <img class="image-rounded" :src="`${postImage.fields.file.url}`" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+
+    <section class="section">
+      <div class="content-wide">
+        <div v-if="cases.fields.finalMockupImage">
+          <img class="image-rounded" :src="`${cases.fields.finalMockupImage.fields.file.url}`" />
+        </div>
+      </div>
+    </section>
+  </div>
+  
 </template>
-
-<style>
-  img {width: 50%;}
-</style>
 
 <script>
   export default {
