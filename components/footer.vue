1196acfb8b9336e3203abfadaddf92fc27da80cc
madebyporter <general@madebyporter.com>
2022-09-28 14:35:03 -0700

    added footer styles. Made header and footer css own files.

---
+++ components/footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="global-footer">
+    <div class="global-footer--copyright">
+      &copy; 2022 Bia
+    </div>
+    <div class="global-footer--rights">
+      All Rights Reserved
+    </div>
+    <div class="global-footer--slogan">
+      <span>Sustainable.</span> 
+      <span class="ssTablet-down-hide">Partners.</span> 
+      <span class="ssTablet-down-hide">Forward.</span>
+    </div>
+  </footer>
+</template>
