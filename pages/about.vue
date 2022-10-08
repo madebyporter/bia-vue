e145abf8ab59e2eda9c61849cddaddf1a963c1d4
madebyporter <general@madebyporter.com>
2022-10-08 14:10:44 -0700

    added accordion styles desktop for about

--- pages/about.vue
+++ pages/about.vue
@@ -23,6 +23,37 @@
         </div>
       </div>
     </section>
+
+    <section class="section-dark">
+      <div class="content content-full">
+        <div class="accordion">
+          <div class="accordion-item closed">
+            <div class="accordion-panel">
+              <h2 class="h1 margin-b-0">who we <span class="s-underline-white">be</span>lieve</h2>
+            </div>
+            <div class="accordion-content">
+              <p>First principle thinkers by design, they find themselves more at home in quantum physics creation myth we are so familiar with.</p>
+            </div>
+          </div>
+          <div class="accordion-item closed">
+            <div class="accordion-panel">
+              <h2 class="h1 margin-b-0">how we <span class="s-underline-white">be</span>have</h2>
+            </div>
+            <div class="accordion-content">
+              <p>First principle thinkers by design, they find themselves more at home in quantum physics creation myth we are so familiar with.</p>
+            </div>
+          </div>
+          <div class="accordion-item opened">
+            <div class="accordion-panel">
+              <h2 class="h1 margin-b-0">why we <span class="s-underline-white">be</span>long</h2>
+            </div>
+            <div class="accordion-content">
+              <p>First principle thinkers by design, they find themselves more at home in quantum physics creation myth we are so familiar with.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
