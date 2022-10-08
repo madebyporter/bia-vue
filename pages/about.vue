d9b1741bc86aad61f36eccd13b5434400d4cd032
madebyporter <general@madebyporter.com>
2022-10-08 13:44:10 -0700

    added founder cards on about

---
+++ pages/about.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <Hero :title="title" description="A collective group of designers, engineers, thinkers, makers creators that get shit done for the better of humanity. Consciousness explained. Dive deeper into our personality and vibe and resonate with us. Based on MBTI and Carl Jung Socionics. We look like Pokemon cards."></Hero>
+    <section class="section">
+      <div class="content content-full sub-grid-thirds">
+        <div class="card card-person">
+          <img class="card-image image-rounded" src="~/assets/img/people/person-stephenmiller.jpg" alt="Stephen Miller" />
+          <h2 class="h2">Stephen Miller</h2>
+          <p class="margin-b-0">ENTP</p>
+          <p class="p-gray">Seeker/inventor</p>
+        </div>
+        <div class="card card-person">
+          <img class="card-image image-rounded" src="~/assets/img/people/person-chrisporter.jpg" alt="Chris Porter" />
+          <h2 class="h2">Chris Porter</h2>
+          <p class="margin-b-0">INTJ</p>
+          <p class="p-gray">Analyst/scientist</p>
+        </div>
+        <div class="card card-person">
+          <img class="card-image image-rounded" src="~/assets/img/people/person-monicathynguyen.jpg" alt="Monica Thy Nguyen" />
+          <h2 class="h2">Monica Thy Nguyen</h2>
+          <p class="margin-b-0">INFJ</p>
+          <p class="p-gray">Humanist/empath</p>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: 'About'
+      }
+    },
+    head() {
+      return {
+        title: this.title,
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: 'Home page description'
+          }
+        ]
+      }
+    },
+  }
+</script>
