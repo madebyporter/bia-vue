d6c015b1f84336cdc422a533617f147ac5de5e92
madebyporter <general@madebyporter.com>
2022-10-08 12:03:10 -0700

    updated footer desktop styles

--- components/footer.vue
+++ components/footer.vue
@@ -1,12 +1,16 @@
 <template>
   <footer class="global-footer">
-    <div class="global-footer--copyright">
+    <div class="global-footer-contact">
+      <p class="p-large">Create a new world together</p>
+      <a class="h1" href="mailto:build@bia.partners">build@bia.partners</a>
+    </div>
+    <div class="global-footer-copyright">
       &copy; 2022 Bia
     </div>
-    <div class="global-footer--rights">
+    <div class="global-footer-rights">
       All Rights Reserved
     </div>
-    <div class="global-footer--slogan">
+    <div class="global-footer-slogan">
       <span>Sustainable.</span> 
       <span class="ssTablet-down-hide">Partners.</span> 
       <span class="ssTablet-down-hide">Forward.</span>
