dd2fe4d8afef744d27adfe41446d3995ba1dac22
Chris Porter <madebyporter@Chriss-MBP.lan>
2022-10-12 11:20:44 -0700

    collective detail desktop styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="section-y-0 hero-home">
-    <div class="content-50 hero-home-left">
+    <div class="content-half hero-home-left">
       <p class="p-large">FuturexForwardxPartners</p>
       <Logo-Tertiary></Logo-Tertiary>
     </div>
-    <div class="content-50 hero-home-right">
+    <div class="content-half hero-home-right">
       <p>Together we work both as a collective and individuals to solve future challenges of humanity. Through our intimate craft, authentic voice, and intricate eye on aesthetics, we deliver the best necessary results for you</p>
     </div>
     <div class="hero-home-img"><img src="~@/assets/img/bg/hero-home-desktop.jpg" alt="BIA Background" /></div>
