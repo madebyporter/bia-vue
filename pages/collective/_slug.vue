be98cc2c7b0ce1b4a4f9a9bf63a13114aabe693e
Chris Porter <madebyporter@Chriss-MBP.lan>
2022-10-12 11:49:16 -0700

    added profile links data to member page

--- pages/collective/_slug.vue
+++ pages/collective/_slug.vue
@@ -24,7 +24,7 @@
         </div>
         <ul v-if="member.fields.links" class="list-default">
           <li v-for="link in member.fields.links">
-            <a :href="`${link}`">{{ link }}</a>
+            <a :href="`${link.fields.linkUrl}`" target="_blank">{{ link.fields.linkType }}</a>
           </li>
         </ul>
       </div>
