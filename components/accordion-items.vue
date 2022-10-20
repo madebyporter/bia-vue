de576d85ac659c54bd85b0d3ae162d99440d9f34
aminaeily <ami.naeily@zeek.com>
2022-10-20 17:57:46 -0600

    if accordion already open close on click

--- components/accordion-items.vue
+++ components/accordion-items.vue
@@ -3,7 +3,7 @@
     <div class="content content-full">
       <div class="accordion">
         <div class="accordion-item" v-for="(faq, i) in content" :key="i">
-          <label @click="active = i" class="accordion-panel">
+          <label @click="active === i ? active = null : active = i" class="accordion-panel">
             <h2 v-html="faq.question" class="accordion-header h1 margin-b-0"></h2>
             <div :class="active === i ? 'accordion-content open': 'accordion-content'">
               <p>{{faq.answer}}</p>
