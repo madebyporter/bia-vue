dfa38a8a4f0889cb6419a37fbdd1aaad4095da2e
madebyporter <general@madebyporter.com>
2022-12-17 14:03:55 -0800

    added staging environment for contentful

--- nuxt.config.js
+++ nuxt.config.js
@@ -99,7 +99,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    "~/plugins/contentful",
+    { src: '~/plugins/contentful', mode: 'client'},
     "~/plugins/members",
     "~/plugins/cases",
     "~/plugins/ventures",
