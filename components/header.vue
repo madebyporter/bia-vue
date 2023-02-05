17dcbf1acebd4de1f9492dc3717bb71d57f90a0d
madebyporter <general@madebyporter.com>
2023-02-05 14:26:54 -0800

    added retractable header

--- components/header.vue
+++ components/header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="global-header">
+  <header ref="nav" class="global-header" :style="{ transform: `translateY(${navTranslateY}px)` }">
     <div class="global-header-logo">
       <NuxtLink to="/" class="global-header-logo-link">
         <Logo :fullLogo="fullLogo"></Logo>
@@ -13,6 +13,28 @@
 
 <script>
   export default {
-    props: ['fullLogo']
+    props: ['fullLogo'],
+    data() {
+      return {
+        navTranslateY: 0,
+        previousScrollY: 0
+      };
+    },
+    mounted() {
+      window.addEventListener("scroll", this.handleScroll);
+    },
+    beforeDestroy() {
+      window.removeEventListener("scroll", this.handleScroll);
+    },
+    methods: {
+      handleScroll() {
+        if (window.scrollY > this.previousScrollY) {
+          this.navTranslateY = -200;
+        } else {
+          this.navTranslateY = 0;
+        }
+        this.previousScrollY = window.scrollY;
+      }
+    }
   }
 </script>
